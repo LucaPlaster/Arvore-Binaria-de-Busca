2da2d820f54adc8a17730bfb38aa50305852ae68
Luca Plaster <lucaplaster@gmail.com>
2022-07-04 09:15:09 -0300

    Quase feito

--- arvoreBinariaDeBusca.c
+++ arvoreBinariaDeBusca.c
@@ -4,58 +4,102 @@
     Gustavo Rodrigues Ribeiro - 202003570
     Luca Mascarenhas Plaster - 202014610
     Maria Heloisa de Paula Souza - 202003599
-
-            fflush(stdin); 
-            continuar = getchar();
-            system("cls");
 */
 
 #include "arvoreBinariaDeBusca.h"
 
 int main(){
-    printf("-----------Arvore Binaria de Busca----------\n");
-
-    int operacao;
-    char continuar;
-    arvoreBinaria *arvore;
 
     criarArvoreVazia();
-
-    printf("0 - Finalizar progama\n");
-    printf("1 - Destruir arvore binaria\n");
-
-    switch (operacao)
-    {
-    case 0:
-        free(arvore);
-        return 0;
-        break;
-
-    case 1:
-        destruirArvore(arvore);
-        break;  
-    
-    default:
-        break;
-    }
+    printf("Aperte ENTER para continuar");
+    fflush(stdin);
+    continuar = getchar();
+    system("cls");
+    funcaoSwitch();
 }
 
-arvoreBinaria *criarArovreVazia(){
+void funcaoSwitch(){
+    int operacao = 1, elemento;
+    arvoreBinaria *arvore =  criarArvoreVazia();
+
+    while(operacao != 0){
+
+        //system ("cls");
+
+        printf("-----------Arvore Binaria de Busca----------\n");
+        printf("0 - Finalizar o progama\n");
+        printf("1 - Exibir arvore binaria\n");
+        printf("2 - Inserir elemento na arvore binaria\n");
+        scanf("%d", &operacao);
+
+
+        switch (operacao)
+        {
+
+        case 1:
+            exibirArvore(arvore);
+            printf("\n");
+            break;
+
+        case 2:
+            printf("\nDigite o elemento que deseja inserir na arvore\n");
+            scanf("%d", &elemento);
+            inserirArvore(&arvore, elemento);
+        
+            break;
+
+        case 4:
+            if (confereArvore(arvore)){
+                printf("\nArvore VAZIA\n");
+            }else printf("Arvore NAO vazia");
+        break;
+
+        }   
+    }
+    destruirArvore(arvore);
+
+    return 0;
+}
+
+arvoreBinaria *criarArvoreVazia(){
     return NULL;
 }
 
 //Destruir a árvore
 void destruirArvore(arvoreBinaria *arv){
-    if(arv != NULL){
-        destruirArvore(arv->dir);
-        destruirArvore(arv->esq);
-        free(arv);
-    }
-    else{
-        printf("Nao ha arvores existentes para serem destruidas\n");
-    }   
-
-    return 0;
+    free (arv);
 }
 
+void exibirArvore(arvoreBinaria *arv){
+    printf("<");
+    if (!confereArvore(arv)){
+        printf("%d", arv->info); //Mostra a raiz
+        exibirArvore(arv->esq); //subárvore à esquerda
+        exibirArvore(arv->dir); //subárvore à direita
+    }
+    printf(">");
+}
 
+int confereArvore(arvoreBinaria *arv){
+    if(arv == NULL) return 1; 
+    else return 0;
+}
+
+void inserirArvore(arvoreBinaria **arv, int elemento){
+    if (*arv ==  NULL){
+        *arv = (arvoreBinaria*)malloc(sizeof(arvoreBinaria));
+        (*arv)->dir = NULL; //Subárvore à direita é NULL 
+        (*arv)->esq = NULL; //Subárvore à esquerda é NULL 
+        (*arv)->info = elemento;//Armazena informação
+
+    }else{
+        if(elemento < (*arv)->info){ //Se o numero for menor vai para a esquerda
+            inserirArvore(&(*arv)->esq, elemento);
+            printf("AAAAA\n");
+        }
+        if(elemento > (*arv)->info){ //Se o numero for maior vai para a direita
+            inserirArvore(&(*arv)->esq, elemento);
+        }
+    }
+
+}
--- arvoreBinariaDeBusca.h
+++ arvoreBinariaDeBusca.h
@@ -7,5 +7,11 @@ typedef struct arvBinaria{
     struct arvBinaria *dir;
 }arvoreBinaria;
 
-arvoreBinaria* criarArvoreVazia();
-void destruirArvore(arvoreBinaria *arvore);
+int continuar;
+
+void funcaoSwitch();                                    //Switch case
+arvoreBinaria* criarArvoreVazia();                      //Cria árvore vazia
+void destruirArvore(arvoreBinaria *arvore);             //Destruir arvore
+void exibirArvore(arvoreBinaria *arvore);               //Mostrar arvore
+int confereArvore(arvoreBinaria *arvore);               //Verificar se arvore esta vazia
+void inserirArvore(arvoreBinaria **arvore, int info);   //Insere um dado na árvore
